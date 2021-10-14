edb5f88107904e1496ace783a8dc83dcb3d42e54
Mav225 <maverickmarsala25@gmail.com>
2021-10-14 11:43:57 -0700

    Create CholeskyMavCode.jl

---
+++ Project1/CholeskyMavCode.jl
@@ -0,0 +1,17 @@
+rows,cols=size(L)               #from the reading of matrix (L) or whatever letter we are using for storing the matrix
+
+a[i,j] = copy(L)
+L=Matrix{Float16}(I,rows,cols)  #storing the matrix for the cholesky
+
+for j=1:cols-1
+    for i=j+1:rows
+        if i != j
+            then                 #go to Matt's matrix math
+            
+        if i == j
+            then
+            k == i
+            C=sqrt(a(j,j)- sum[L(i,k) * L(j,k) | k == [0:j-1]])
+            L[i,j]=C
+    end
+end
